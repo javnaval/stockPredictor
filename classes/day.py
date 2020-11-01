123d412c14daa435ee38114cc0e31941e4d4fe3d
Javier Navalón <36413838+javnaval@users.noreply.github.com>
2020-11-01 16:30:17 +0100

    okman

---
+++ classes/day.py
@@ -0,0 +1,4 @@
+class Day:
+
+    def f(self):
+        return 'hello puto'
--- init.py
+++ init.py
@@ -2,8 +2,10 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 import cProfile
-import json
+import json, urllib
+import "../day.py"
 
+
+dia = Day()
+dia.f()
 input(" press close to exit ")
-http://data.fixer.io/api/2013-12-24debe3f0a16020d511774fbf41a15d04fGBPUSD
-
